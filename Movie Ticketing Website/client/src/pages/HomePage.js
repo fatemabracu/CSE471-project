5d49c5a149acd3bd30c38a67fa05bbe52add8553
Siam <135495410+iams1am@users.noreply.github.com>
2025-04-25 10:57:45 +0600

    Add files via upload

---
+++ Movie Ticketing Website/client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const HomePage = () => {
+  return (
+    <Layout tittle= {'Book Now - Movie Ticketing'}>
+      <h1>HomePage</h1>
+    </Layout>
+  );
+};
+
+export default HomePage;
